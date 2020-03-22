6846de43ff589414131923002a1df2d492810af7
nadinho <rockt2935@gmail.com>
2020-03-22 13:42:08 +0100

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import GlobalStyles from './Globalstyles';
+import styled from '@emotion/styled';
+
+const SpeechBubble = styled.div`
+  height: 300px;
+  width: 450px;
+  position: absolute;
+  top: 70px;
+  left: 50px;
+  background: darkgrey;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 4px solid darkgrey;
+  border-radius: 40px;
+  /* -moz-border-radius: 40px;
+  -webkit-border-radius: 40px; */
+  &::before {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-top: 33px solid transparent;
+    border-left: 46px solid darkgrey;
+    border-bottom: 33px solid transparent;
+    margin: -53px 0 0 248px;
+  }
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      Hello World
+
+      <SpeechBubble>Here come's the API</SpeechBubble>
     </>
   );
 }
--- src/Globalstyles.js
+++ src/Globalstyles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Wallpaper from './assets/chuck-norris-wallpaper.jpg';
 
 export default function GlobalStyles() {
   return (
@@ -13,8 +14,10 @@ export default function GlobalStyles() {
         body {
           margin: 0;
           height: 100vh;
-          font-size: 1rem;
-          background: grey;
+          font-size: 1.5rem;
+          color: white;
+          background-image: url(${Wallpaper});
+          background-size: cover;
         }
       `}
     />
