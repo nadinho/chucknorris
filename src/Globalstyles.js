86ddb02e4681d9eade5886a32cc1dfbba6b55894
Nadine Gehrmann <57504815+nadinho@users.noreply.github.com>
2020-03-22 12:39:36 +0100

    Dev Setup (#1)
    * add prettier
    
    * add json-server and react-router
    
    * remove starter code
    
    * add starter globalstyle

---
+++ src/Globalstyles.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={(theme) => css`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+        body {
+          margin: 0;
+          height: 100vh;
+          font-size: 1rem;
+          background: grey;
+        }
+      `}
+    />
+  );
+}
