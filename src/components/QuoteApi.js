3918674f2d7021b41300246b343ec9a2d8cf70eb
Nadine Gehrmann <57504815+nadinho@users.noreply.github.com>
2020-03-23 08:25:09 +0100

    Restyling (#5)
    * remove background image
    
    * fix speech bubble
    
    * little changes

--- src/components/QuoteApi.js
+++ src/components/QuoteApi.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Button from './Button';
-// import styled from '@emotion/styled';
+// import React from 'react';
+// import Button from './Button';
+// // import styled from '@emotion/styled';
 
-export default function randomQuote() {
-  const [quote, setQuote] = use.State('');
+// export default function randomQuote() {
+//   const [quote, setQuote] = use.State('');
 
-  React.useEffect(() => {
-    async function getQuote() {
-      const response = await fetch('https://api.chucknorris.io/jokes/random');
-      const results = await response.json();
-      const quote = results.value;
-      setQuote(quote);
-    }
-    getQuote(quote);
-  });
+//   React.useEffect(() => {
+//     async function getQuote() {
+//       const response = await fetch('https://api.chucknorris.io/jokes/random');
+//       const results = await response.json();
+//       const quote = results.value;
+//       setQuote(quote);
+//     }
+//     getQuote(quote);
+//   });
 
-  return (
-    <>
-      <Button onClick={randomQuote()}></Button>
-    </>
-  );
-}
+//   return (
+//     <>
+//     <
+//       <Button onClick={randomQuote()}></Button>
+//     </>
+//   );
+// }
